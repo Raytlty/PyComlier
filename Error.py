35543a367e4dd14a656dcbc71b3a28ae9a4b321b
BencoLee <alexmao930918@gmail.com>
2015-03-01 20:15:29 +0800

    Revert "Complete Complier"
    This reverts commit bc2a3ff0452b299cc97343270465b8d57433e380.

--- Error.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'Benco'
-
-def CerrError(num,lineNo,lexeme):
-    if num==1:
-        raise Exception(lineNo,"Wrong Masked",lexeme)
-    elif num==2:
-        raise Exception(lineNo,"Not Want Masked",lexeme)
-    elif num==3:
-        raise Exception(lineNo,"No Match Masked",lexeme)
